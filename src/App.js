0e243d1640fdd9fd21d8a4b1699cbff1e9452810
Mastanee <mastane.kachloo@gmail.com>
2024-09-15 21:42:18 +0200

    Add sources

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ function App() {
   return (
     <div className="App">
       <Weather />
+      <p>
+        {" "}
+        This project was coded by{" "}
+        <a href="https://www.shecodes.io/graduates/51244-mastane-kachloo">
+          Mastane Kachloo
+        </a>{" "}
+        and it is open-sourced on
+        <a href="https://github.com/Mastanee/react-weather-app">Github</a> and
+        hosted on <a href="https://peaceful-palmier-0b7b31.netlify.app"></a>{" "}
+        Netlify.
+      </p>
     </div>
   );
 }
